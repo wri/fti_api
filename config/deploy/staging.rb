71c369e803b60db6858677a5d93f97d70dd8e8d1
rschumann <sebastian.schkudlara@gmail.com>
2017-04-24 13:33:23 +0200

    Changes for deploy files

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -75,7 +75,7 @@ set :deploy_to, '~/fti-api-staging'
 
 set :default_env, {
   'PATH' => "/home/ubuntu/.rvm/gems/ruby-2.4.0/bin:/home/ubuntu/.rvm/bin:$PATH",
-  'RUBY_VERSION' => 'ruby-2.2.0',
+  'RUBY_VERSION' => 'ruby-2.4.0',
   'GEM_HOME'     => '/home/ubuntu/.rvm/gems/ruby-2.4.0',
   'GEM_PATH'     => '/home/ubuntu/.rvm/gems/ruby-2.4.0',
   'BUNDLE_PATH'  => '/home/ubuntu/.rvm/gems/ruby-2.4.0'
