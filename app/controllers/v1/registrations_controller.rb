9d4cddd42a369cdae842e54ace76caeda1d8410e
rschumann <sebastian.schkudlara@gmail.com>
2017-04-27 17:03:52 +0200

    Fixes for downcase user permissions params

--- app/controllers/v1/registrations_controller.rb
+++ app/controllers/v1/registrations_controller.rb
@@ -19,7 +19,7 @@ module V1
 
       def user_params
         params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation, :permissions_request, :country_id, :institution).tap do |user_params|
-          user_params[:permissions_request] = params[:user][:permissions_request].downcase
+          user_params[:permissions_request] = params[:user][:permissions_request].downcase if params[:user][:permissions_request].present?
         end
       end
   end
