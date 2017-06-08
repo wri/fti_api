8cbf9a3a2aab5ee3d7c63ff95dd3ae0f8aa5cdae
Tiago Santos <santos.tiago@gmail.com>
2017-06-08 19:04:03 +0200

    Changed the observation filters endpoit to a format that's friendlier to the frontend

--- app/controllers/v1/observation_filters_controller.rb
+++ app/controllers/v1/observation_filters_controller.rb
@@ -7,17 +7,19 @@ module V1
     skip_before_action :authenticate
 
     def index
-      annexes = %w(AnnexOperator AnnexGovernance)
+      annexes = [{id: 'AnnexOperator', name: 'Operator'}, {id: 'AnnexGovernance', name: 'Governance'}]
       countries = Country.all.includes(:translations).with_translations(I18n.available_locales).pluck(:id, :iso, :name)
         .map{|x| {id: x[0], iso: x[1], name: x[2]}}
       fmus = Fmu.all.includes(:translations).with_translations(I18n.available_locales).pluck(:id, :name)
-        .map{|x| {id: x[0], name: x[0]}}
+        .map{|x| {id: x[0], name: x[1]}}
       years = Observation.pluck(:publication_date).map{|x| x.year}.uniq.sort
+        .map{ |x| {id: x, name: x }}
       monitors = Observer.all.includes(:translations).with_translations(I18n.available_locales).pluck(:id, :name)
         .map{|x| {id: x[0], name: x[1]}}
       categories = Category.all.includes(:translations).with_translations(I18n.available_locales).pluck(:id, :name)
         .map{|x| {id: x[0], name: x[1]}}
-      levels = Severity.all.includes(:translations).with_translations(I18n.available_locales).pluck(:level).sort
+      #levels = Severity.all.includes(:translations).with_translations(I18n.available_locales).pluck(:level).sort
+      levels =[{id: 0, name: 'Unknown'}, {id: 1, name: 'Low'}, {id: 2, name: 'Medium'}, {id: 3, name: 'High'}]
 
       filters = {
           'type': annexes,
