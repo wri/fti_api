9ab82c07a3e7abb56859d81ac1b97fe0c5bc80f8
Tiago Santos <santos.tiago@gmail.com>
2017-06-19 17:13:31 +0200

    Changed the observation_filters names

--- app/controllers/v1/observation_filters_controller.rb
+++ app/controllers/v1/observation_filters_controller.rb
@@ -8,27 +8,27 @@ module V1
 
     def index
       annexes = [{id: 'operator', name: 'Operator'}, {id: 'governance', name: 'Governance'}]
-      countries = Country.all.includes(:translations).with_translations(I18n.available_locales).pluck(:id, :iso, :name)
+      country_ids = Country.all.includes(:translations).with_translations(I18n.available_locales).pluck(:id, :iso, :name)
         .map{|x| {id: x[0], iso: x[1], name: x[2]}}
-      fmus = Fmu.all.includes(:translations).with_translations(I18n.available_locales).pluck(:id, :name)
+      fmu_ids = Fmu.all.includes(:translations).with_translations(I18n.available_locales).pluck(:id, :name)
         .map{|x| {id: x[0], name: x[1]}}
       years = Observation.pluck(:publication_date).map{|x| x.year}.uniq.sort
         .map{ |x| {id: x, name: x }}
-      monitors = Observer.all.includes(:translations).with_translations(I18n.available_locales).pluck(:id, :name)
+      monitor_ids = Observer.all.includes(:translations).with_translations(I18n.available_locales).pluck(:id, :name)
         .map{|x| {id: x[0], name: x[1]}}
-      categories = Category.all.includes(:translations).with_translations(I18n.available_locales).pluck(:id, :name)
+      category_ids = Category.all.includes(:translations).with_translations(I18n.available_locales).pluck(:id, :name)
         .map{|x| {id: x[0], name: x[1]}}
       #levels = Severity.all.includes(:translations).with_translations(I18n.available_locales).pluck(:level).sort
-      levels =[{id: 0, name: 'Unknown'}, {id: 1, name: 'Low'}, {id: 2, name: 'Medium'}, {id: 3, name: 'High'}]
+      severities =[{id: 0, name: 'Unknown'}, {id: 1, name: 'Low'}, {id: 2, name: 'Medium'}, {id: 3, name: 'High'}]
 
       filters = {
           'types': annexes,
-          'countries': countries,
-          'fmus': fmus,
+          'country_ids': country_ids,
+          'fmu_ids': fmu_ids,
           'years': years,
-          'monitors': monitors,
-          'categories': categories,
-          'levels': levels
+          'monitor_ids': monitor_ids,
+          'category_ids': category_ids,
+          'severities': severities
       }.to_json
 
       render json: filters
