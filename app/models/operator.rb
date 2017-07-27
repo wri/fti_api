5e71d996243f6b5a1793329785603f42e3a5c9b4
Tiago Santos <santos.tiago@gmail.com>
2017-07-27 13:02:53 +0200

    Changed the updated_valid_documents_percentages

--- app/models/operator.rb
+++ app/models/operator.rb
@@ -74,11 +74,11 @@ class Operator < ApplicationRecord
   end
 
   def update_valid_documents_percentages
-    all = (operator_documents.where(status: OperatorDocument.statuses[:doc_valid]).count / operator_documents.count).to_f rescue 0
-    fmu = (operator_documents.where(type: 'OperatorDocumentFmu', status: OperatorDocument.statuses[:doc_valid]).count / operator_documents.where(type: 'OperatorDocumentFmu').count).to_f rescue 0
-    country = (operator_documents.where(type: 'OperatorDocumentCountry', status: OperatorDocument.statuses[:doc_valid]).count / operator_documents.where(type: 'OperatorDocumentCountry').count).to_f rescue 0
+    percentage_valid_documents_all = (operator_documents.where(status: OperatorDocument.statuses[:doc_valid]).count / operator_documents.count).to_f rescue 0
+    percentage_valid_documents_fmu = (operator_documents.where(type: 'OperatorDocumentFmu', status: OperatorDocument.statuses[:doc_valid]).count / operator_documents.where(type: 'OperatorDocumentFmu').count).to_f rescue 0
+    percentage_valid_documents_country = (operator_documents.where(type: 'OperatorDocumentCountry', status: OperatorDocument.statuses[:doc_valid]).count / operator_documents.where(type: 'OperatorDocumentCountry').count).to_f rescue 0
 
-    self.update_attributes(percentage_valid_documents_all: all, percentage_valid_documents_country: country, percentage_valid_documents_fmu: fmu)
+    save
   end
 
   private
