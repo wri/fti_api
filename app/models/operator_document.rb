48d90482e9654c11a0384377eca5b6839d91009a
Tiago Santos <santos.tiago@gmail.com>
2017-07-29 19:04:23 +0200

    Added expire date calculations

--- app/models/operator_document.rb
+++ app/models/operator_document.rb
@@ -15,17 +15,24 @@
 #
 
 class OperatorDocument < ApplicationRecord
-  belongs_to :operator
-  belongs_to :required_operator_document
+  belongs_to :operator, required: true
+  belongs_to :required_operator_document, required: true
 
-  has_many :documents, as: :attacheable
-  accepts_nested_attributes_for :documents
+  has_one :document, as: :attacheable
+  accepts_nested_attributes_for :document
 
+  before_validation :set_expire_date, unless: :expire_date_changed?
+  validates_presence_of :start_date
+  validates_presence_of :expire_date
   after_save :update_operator_percentages, if: :status_changed?
 
   enum status: { doc_not_provided: 0, doc_pending: 1, doc_invalid: 2, doc_valid: 3, doc_expired: 4 }
 
   def update_operator_percentages
     operator.update_valid_documents_percentages
+  end
+
+  def set_expire_date
+    self.expire_date = start_date + required_operator_document.valid_period.days rescue start_date
   end
 end
