642a524a680d33ce98171a95417ad37b66ae2745
Tiago Santos <santos.tiago@gmail.com>
2017-08-04 16:10:36 +0200

    Added AAG to Operators

--- app/admin/operator.rb
+++ app/admin/operator.rb
@@ -2,9 +2,11 @@
 ActiveAdmin.register Operator do
 
   actions :all, except: :destroy
-  permit_params :name, :country_id, :details, :concession, :is_active, :certification
+  permit_params :name, :country_id, :details, :concession, :is_active, :certification,
+                translations_attributes: [:id, :locale, :name, :details, :destroy]
 
   index do
+    translation_status
     column :country
     column :name
     column :concession
@@ -19,11 +21,15 @@ ActiveAdmin.register Operator do
 
   form do |f|
     f.semantic_errors *f.object.errors.keys
+    f.inputs 'Translated fields' do
+      f.translated_inputs switch_locale: false do |t|
+        t.input :name
+        t.input :details
+      end
+    end
     f.inputs 'Country Details' do
       f.input :country
-      f.input :name
       f.input :certification
-      f.input :details
       f.input :concession
       f.input :logo
       f.input :is_active
