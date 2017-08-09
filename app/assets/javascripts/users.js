1a5a2f3c0888df822d8297a4b00617c5d275f1cf
Tiago Santos <santos.tiago@gmail.com>
2017-08-09 12:40:58 +0200

    Hid the fields in ActiveAdmin Users that aren't needed

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,39 @@
+$(document).on('ready', function() {
+  updateFields();
+  $('#user_user_permission_attributes_user_role').on('change', function(){
+    updateFields();
+  })
+})
+
+function updateFields(){
+  var userRole = $('#user_user_permission_attributes_user_role').val();
+  var observerInput = $('#user_observer_id');
+  var operatorInput = $('#user_operator_id');
+
+  switch (userRole) {
+    case 'admin':
+      observerInput.prop('disabled', true);
+      observerInput.parent().hide();
+      operatorInput.prop('disabled', true);
+      operatorInput.parent().hide();
+      break;
+    case 'ngo':
+      observerInput.prop('disabled', false);
+      observerInput.parent().show();
+      operatorInput.prop('disabled', true);
+      operatorInput.parent().hide();
+      break;
+    case 'operator':
+      observerInput.prop('disabled', true);
+      observerInput.parent().hide();
+      operatorInput.prop('disabled', false);
+      operatorInput.parent().show();
+      break;
+    case 'user':
+      observerInput.prop('disabled', true);
+      observerInput.parent().hide();
+      operatorInput.prop('disabled', true);
+      operatorInput.parent().hide();
+      break;
+  }
+}
