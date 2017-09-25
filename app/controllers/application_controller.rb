6f3c18e44c952457fcc511b7e8d09e093a75a15c
Tiago Santos <santos.tiago@gmail.com>
2017-09-25 17:31:33 +0100

    Added user activation and deactivation to the active admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ class ApplicationController < ActionController::Base
   # Active admin permissions
   def authenticate_user!
     if current_user.present?
-      raise SecurityError unless current_user.user_permission.present? && current_user.user_permission.user_role == 'admin'
+      unless current_user.user_permission.present? && current_user.user_permission.user_role == 'admin' && current_user.is_active
+        raise SecurityError
+      end
     else
       redirect_to user_session_path
     end
