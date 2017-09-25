ecbbcddea1dd2d95539836236e14fee0bad2d11d
Tiago Santos <santos.tiago@gmail.com>
2017-09-25 17:16:07 +0100

    Only admin users can log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,16 @@
 
 class ApplicationController < ActionController::Base
 
+  # Active admin permissions
+  def authenticate_user!
+    if current_user.present?
+      raise SecurityError unless current_user.user_permission.present? && current_user.user_permission.user_role == 'admin'
+    else
+      redirect_to user_session_path
+    end
+  end
+
+  rescue_from SecurityError do
+    redirect_to destroy_user_session_path
+  end
 end
