283d0f65bc7e6f7083b69ef94ca54e671d31c4d4
Tiago Santos <santos.tiago@gmail.com>
2017-09-26 16:59:30 +0200

    Added new sorts for the law resources

--- app/resources/v1/law_resource.rb
+++ app/resources/v1/law_resource.rb
@@ -11,6 +11,10 @@ module V1
     filters :country, :subcategory, :written_infraction, :infraction, :sanctions, :min_fine, :max_fine,
             :penal_servitude, :other_penalties, :apv
 
+    def self.sortable_fields(context)
+      super + [:'subcategory.name', :'country.name']
+    end
+
     def custom_links(_)
       { self: nil }
     end
