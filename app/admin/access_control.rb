221176cfa33b47ba5a711b3390d16b83b8930e0a
Tiago Santos <santos.tiago@gmail.com>
2017-10-05 18:53:59 +0200

    Finished the access control form

--- app/admin/access_control.rb
+++ app/admin/access_control.rb
@@ -1,21 +1,7 @@
 ActiveAdmin.register_page 'Access Control' do
   menu parent: 'User Management', priority: 2
-  # content do
-  #   render partial: 'manager',
-  #          locals: { users: User.joins(:user_permission).
-  #              where("user_permissions.user_role = #{UserPermission.user_roles[:bo_manager]}") }
-  # end
 
-  # content do
-  #   if params["edit"] == "true"
-  #     render partial:'form'
-  #   else
-  #     render partial:'body'
-  #   end
-  # end
-
-  page_action :edit do |id|
-    puts 'cenas'
+  page_action :edit do
   end
 
   controller do
@@ -27,10 +13,5 @@ ActiveAdmin.register_page 'Access Control' do
       @up = User.find(params['format']).user_permission
       render layout: 'active_admin'
     end
-
-    def new
-    end
   end
-
-
 end
