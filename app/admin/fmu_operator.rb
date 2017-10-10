dc7dd1de1ed93d8f35a426c5d3305f8084435f46
Tiago Santos <santos.tiago@gmail.com>
2017-10-10 16:58:34 +0200

    Small changes to AA to account for the changes of fmu operator

--- app/admin/fmu_operator.rb
+++ app/admin/fmu_operator.rb
@@ -4,4 +4,29 @@ ActiveAdmin.register FmuOperator do
   actions :show, :edit, :index, :update, :new, :create
 
   permit_params :fmu_id, :operator_id, :current, :start_date, :end_date
+
+  index do
+    column :current
+    column :fmu
+    column :operator
+    column :start_date
+    column :end_date
+
+    actions
+  end
+
+  form do |f|
+    edit = f.object.new_record? ? false : true
+    f.semantic_errors *f.object.errors.keys
+
+    f.inputs do
+      f.input :fmu, as: :select, input_html: { disabled: edit }
+      f.input :operator, as: :select, input_html: { disabled: edit }
+      f.input :start_date, as: :date_time_picker, picker_options: { timepicker: false }
+      f.input :end_date, as: :date_time_picker, picker_options: { timepicker: false }
+      f.input :current
+    end
+
+    f.actions
+  end
 end
