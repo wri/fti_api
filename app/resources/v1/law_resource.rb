f634cf489ed85f5ed10089a915da3b9f23881c6a
Tiago Santos <santos.tiago@gmail.com>
2017-10-16 18:13:02 +0200

    Added field  to the laws endpoint

--- app/resources/v1/law_resource.rb
+++ app/resources/v1/law_resource.rb
@@ -2,7 +2,7 @@ module V1
   class LawResource < JSONAPI::Resource
     caching
     attributes :written_infraction, :infraction, :sanctions, :min_fine, :max_fine,
-               :penal_servitude, :other_penalties, :apv
+               :penal_servitude, :other_penalties, :apv, :complete
 
     has_one :subcategory
     has_one :country
@@ -14,6 +14,30 @@ module V1
     def self.sortable_fields(context)
       super + [:'subcategory.name', :'country.name']
     end
+
+    def complete
+      @model.written_infraction.present? &&
+          @model.infraction.present? &&
+          @model.sanctions.present? &&
+          @model.min_fine.present? &&
+          @model.max_fine.present? &&
+          @model.penal_servitude.present? &&
+          @model.apv.present?
+    end
+
+
+      #  written_infraction :text
+      #  infraction         :text
+      #  sanctions          :text
+      #  min_fine           :integer
+      #  max_fine           :integer
+      #  penal_servitude    :string
+      #  other_penalties    :text
+      #  apv                :text
+      #  subcategory_id     :integer
+      #  created_at         :datetime         not null
+      #  updated_at         :datetime         not null
+      #  country_id         :integer
 
     def custom_links(_)
       { self: nil }
