812f6f4ffbd6c36eceaa5776dba126f4167253da
Tiago Santos <santos.tiago@gmail.com>
2017-10-17 10:23:38 +0200

    Removed the code that would never send the geojson in the FMU resource to the observations-tool. It will be needed from now on, so it can be filtered by using

--- app/resources/v1/fmu_resource.rb
+++ app/resources/v1/fmu_resource.rb
@@ -10,14 +10,5 @@ module V1
     def custom_links(_)
       { self: nil }
     end
-
-    def fetchable_fields
-      if (context[:app] != 'observations-tool')
-        super - [:geojson]
-      else
-        super
-      end
-    end
-
   end
 end
