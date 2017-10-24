9757f939993b1c6ba58707f27fe2fbef7dde7cc3
Tiago Santos <santos.tiago@gmail.com>
2017-10-24 22:58:20 +0200

    Added the creation of the a new operator document when removing an active ont

--- app/models/operator_document.rb
+++ app/models/operator_document.rb
@@ -85,8 +85,11 @@ class OperatorDocument < ApplicationRecord
 
   def insure_unity
     if self.current && self.required_operator_document.present?
-    else
-      true
+      od = OperatorDocument.new(fmu_id: self.fmu_id, operator_id: self.operator_id,
+                                required_operator_document_id: self.required_operator_document_id,
+                                status: OperatorDocument.statuses[:doc_not_provided], type: self.type,
+                                current: true)
+      od.save!(validate: false)
     end
   end
 
