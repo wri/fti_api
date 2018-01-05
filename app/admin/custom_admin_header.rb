805b2e2cf6b6b24f532f7735529c51efab11bebb
Tiago Santos <santos.tiago@gmail.com>
2018-01-05 14:23:37 +0000

    Added the menu structure required by the client

--- app/admin/custom_admin_header.rb
+++ app/admin/custom_admin_header.rb
@@ -14,8 +14,21 @@ class CustomAdminHeader < ActiveAdmin::Views::Header
         li do
           text_node 'Independent Monitoring'
           ul do
-            li { link_to 'Monitors', admin_monitors_path }
-            # If you want to add more children, including more LIs here.
+            li { link_to 'Monitors',         admin_monitors_path }
+            li { link_to 'Observations',     admin_observations_path }
+            li { link_to 'Reports',          admin_observation_reports_path }
+            li { link_to 'Evidence',         admin_evidences_path }
+            li do
+              text_node link_to 'Settings'
+              ul do
+                li { link_to 'Categories',             admin_categories_path }
+                li { link_to 'Subcategories',          admin_subcategories_path }
+                li { link_to 'Severities',             admin_severities_path }
+                li { link_to 'Laws',                   admin_laws_path }
+                li { link_to 'Government Entities',    admin_governments_path }
+                li { link_to 'Species',                admin_species_index_path }
+              end
+            end
           end
         end
       end
@@ -25,13 +38,32 @@ class CustomAdminHeader < ActiveAdmin::Views::Header
         li do
           text_node 'Private Sector'
           ul do
+            li { link_to 'Producers',            admin_producers_path }
+            li { link_to 'Document Categories',  admin_required_operator_document_groups_path }
+            li { link_to 'Required Documents',   admin_required_operator_documents_path }
+            li { link_to 'Producer Documents',   admin_operator_documents_path }
+            li { link_to 'Annexes',              admin_operator_document_annexes_path }
             li do
-              text_node link_to('Producers', admin_producers_path)
+              text_node link_to 'Settings'
               ul do
-                li { link_to 'Test', admin_observations_path }
-                # If you want to add more grandchildren, including more LIs here.
+                li { link_to 'Countries',            admin_countries_path }
+                li { link_to 'Fmus',                 admin_fmus_path }
+                li { link_to 'Fmu allocations',      admin_fmu_operators_path }
               end
             end
+          end
+        end
+      end
+
+      ul do
+        li do
+          text_node 'User Management'
+          ul do
+            li { link_to 'Users',            admin_users_path }
+            li { link_to 'Access Control',   admin_access_control_path }
+            li { link_to 'Contacts',         admin_contacts_path }
+            li { link_to 'Partners',         admin_partners_path }
+            li { link_to 'Comments',         admin_comments_path }
           end
         end
       end
