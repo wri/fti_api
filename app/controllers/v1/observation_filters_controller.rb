2411c0aa22b949f43cb949b7bd4346c64db02039
Tiago Santos <santos.tiago@gmail.com>
2018-01-17 18:51:27 +0000

    Added translations for observation filters

--- app/controllers/v1/observation_filters_controller.rb
+++ app/controllers/v1/observation_filters_controller.rb
@@ -8,7 +8,7 @@ module V1
     skip_before_action :authenticate
 
     def index
-      types = [{ id: 'operator', name: 'Operator' }, { id: 'government', name: 'Governance' }]
+      types = [{ id: 'operator', name: I18n.t('filters.operator') }, { id: 'government', name: I18n.t('filters.governance') }]
       country_ids = Country.with_observations.joins(:translations)
                         .map{|x| { id: x.id, iso: x.iso, name: x.name }}.sort_by { |c| c[:name] }
       fmu_ids = Fmu.all.joins(:translations).map{|x| { id: x.id, name: x.name }}
@@ -16,7 +16,11 @@ module V1
                   .map{ |x| { id: x, name: x }}
       observer_ids = Observer.all.includes(:translations).map{|x| { id: x.id, name: x.name }}
       category_ids = Category.all.includes(:translations).map{ |x| { id: x.id, name: x.name } }
-      severities =[{ id: 0, name: 'Unknown' }, { id: 1, name: 'Low' }, { id: 2, name: 'Medium' }, { id: 3, name: 'High' }]
+      severities =[
+          { id: 0, name: I18n.t('filters.unknown') },
+          { id: 1, name: I18n.t('filters.low') },
+          { id: 2, name: I18n.t('filters.medium') },
+          { id: 3, name: I18n.t('filters.high') }]
 
       filters = {
           'observation_type': types,
