11930461600b8cbf7e1b9b6ee90571b125e7767a
Tiago Santos <santos.tiago@gmail.com>
2018-01-18 09:36:24 +0000

    In the observations filters added the report and sorting

--- app/controllers/v1/observation_filters_controller.rb
+++ app/controllers/v1/observation_filters_controller.rb
@@ -10,17 +10,20 @@ module V1
     def index
       types = [{ id: 'operator', name: I18n.t('filters.operator') }, { id: 'government', name: I18n.t('filters.governance') }]
       country_ids = Country.with_observations.joins(:translations)
-                        .map{|x| { id: x.id, iso: x.iso, name: x.name }}.sort_by { |c| c[:name] }
-      fmu_ids = Fmu.all.joins(:translations).map{|x| { id: x.id, name: x.name }}
+                        .map{|x| { id: x.id, iso: x.iso, name: x.name }}.sort_by { |x| x[:name] }
+      fmu_ids = Fmu.all.joins(:translations).map{|x| { id: x.id, name: x.name }}.sort_by { |x| x[:name] }
       years = Observation.pluck(:publication_date).map{|x| x.year}.uniq.sort
                   .map{ |x| { id: x, name: x }}
-      observer_ids = Observer.all.includes(:translations).map{|x| { id: x.id, name: x.name }}
-      category_ids = Category.all.includes(:translations).map{ |x| { id: x.id, name: x.name } }
+      observer_ids = Observer.all.includes(:translations).map{|x| { id: x.id, name: x.name }}.sort_by { |x| x[:name] }
+      category_ids = Category.all.includes(:translations).map{ |x| { id: x.id, name: x.name } }.sort_by { |x| x[:name] }
       severities =[
           { id: 0, name: I18n.t('filters.unknown') },
           { id: 1, name: I18n.t('filters.low') },
           { id: 2, name: I18n.t('filters.medium') },
           { id: 3, name: I18n.t('filters.high') }]
+      operator_ids = Operator.active.includes(:translations).map {|x| { id: x.id, name: x.name }}.sort_by { |x| x[:name] }
+      reports_ids = ObservationReport.all.map { |x| { id: x.id, name: x.title }}.sort_by { |x| x[:title] }
+
 
       filters = {
           'observation_type': types,
@@ -29,7 +32,9 @@ module V1
           'years': years,
           'observer_id': observer_ids,
           'category_id': category_ids,
-          'severity_level': severities
+          'severity_level': severities,
+          operator_id: operator_ids,
+          report_id: reports_ids
       }.to_json
 
       render json: filters
