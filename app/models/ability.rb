d5ee40a7111a4f6f7fb8c5e4cddb0c6ca1370d48
Tiago Santos <santos.tiago@gmail.com>
2018-01-24 16:33:36 +0000

    Added the ability to read all sawmills

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -39,7 +39,7 @@ class Ability
 
     can :read, [Country, Fmu, Category, Subcategory, Law, Species,
                 OperatorDocument, RequiredOperatorDocument, RequiredOperatorDocumentGroup,
-                ObservationReport, ObservationDocument, Partner, OperatorDocumentAnnex]
+                ObservationReport, ObservationDocument, Partner, OperatorDocumentAnnex, Sawmill]
     can :read, Observation, is_active: true
     can :read, Observer, is_active: true
     can :read, Operator, is_active: true
