5b64fb85c005e89c4202dbfaed14bed8c1da8ea1
Tiago Santos <santos.tiago@gmail.com>
2018-01-26 10:08:35 +0000

    Changed the sawmill enpoint to accept a format geojson

--- app/models/sawmill.rb
+++ app/models/sawmill.rb
@@ -25,20 +25,28 @@ class Sawmill < ApplicationRecord
 
   after_save :update_geojson
 
+
+  scope :filter_by_operators,      ->(operator_ids) { where( operator_id: operator_ids.split(','))  }
+
+  def self.fetch_all(options)
+    operator_ids  = options['operator_ids'] if options.present? && options['operator_ids'].present? && ValidationHelper.ids?(options['operator_ids'])
+    active  = true if options.present? && !options['active'].nil?
+
+    sawmills = includes(:operator)
+    sawmills = sawmills.active if active
+    sawmills = sawmills.filter_by_operators(operator_ids) if operator_ids.present?
+    sawmills
+  end
+
   private
 
   def update_geojson
     query = "with subquery as(
                 select id, json_build_object(
-                'type', 'FeatureCollection',
-                'features', json_agg(
-                    json_build_object(
                         'type', 'Feature',
                         'id', id,
                         'geometry', ST_AsGeoJSON(ST_MakePoint(lng, lat))::json,
                        'properties', (select row_to_json(sub) from (select name, is_active, operator_id) as sub)
-                        )
-                    )
               ) as geojson
               from sawmills
               where id = #{self.id}
