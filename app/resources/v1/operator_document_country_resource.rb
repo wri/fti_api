d0fff28f84abf7dfb8e972534d335f67f928499b
Tiago Santos <santos.tiago@gmail.com>
2019-07-04 16:57:48 +0100

    Changed the label for forest_types to be the same as in FA. Minor fixes to BO for forest-types

--- app/resources/v1/operator_document_country_resource.rb
+++ app/resources/v1/operator_document_country_resource.rb
@@ -2,14 +2,19 @@
 
 module V1
   class OperatorDocumentCountryResource < OperatorDocumentResource
-    attributes :expire_date, :start_date,
-               :status, :created_at, :updated_at,
-               :attachment, :operator_id, :required_operator_document_id,
-               :current, :reason
-
     has_one :required_operator_document_country
     has_many :documents
-    has_many :operator_document_annexes
 
+    def fetchable_fields
+      super - [:fmu_id]
+    end
+
+    def self.updatable_fields(context)
+      super - [:fmu_id]
+    end
+
+    def self.creatable_fields(context)
+      super - [:fmu_id]
+    end
   end
 end
--- app/resources/v1/operator_document_fmu_resource.rb
+++ app/resources/v1/operator_document_fmu_resource.rb
@@ -2,13 +2,12 @@
 
 module V1
   class OperatorDocumentFmuResource < OperatorDocumentResource
-
     has_one :required_operator_document_fmu
     has_many :documents
 
     def forest_type
       rod = @model.required_operator_document
-      Fmu::FOREST_TYPES[rod.forest_type.to_sym][:label] if rod.forest_type
+      Fmu::FOREST_TYPES[rod.forest_type.to_sym][:geojson_label] if rod.forest_type
     end
   end
 end
