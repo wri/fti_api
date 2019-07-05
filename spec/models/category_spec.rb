e0ef669ca87a013255f2b19a4c7e0a31036154d9
Jesús González <jesus198927@gmail.com>
2019-07-05 17:44:09 +0200

    Add test for category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -12,42 +12,33 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   before :each do
-    I18n.locale = :en
     FactoryGirl.create(:category, name: 'Z Category')
     @category = create(:category, name: '00 Category')
   end
 
-  it 'Count on category' do
-    expect(Category.count).to          eq(2)
-    expect(Category.all.first.name).to eq('Z Category')
+  it_should_behave_like 'translatable', FactoryGirl.create(:category), %i[name]
+
+  describe 'Relations' do
+    it { is_expected.to have_many(:subcategories).dependent(:destroy) }
   end
 
-  it 'Order by name asc' do
-    expect(Category.by_name_asc.first.name).to eq('00 Category')
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:name) }
   end
 
-  it 'Fallbacks for empty translations on category' do
-    I18n.locale = :fr
-    expect(@category.name).to eq('00 Category')
-    I18n.locale = :en
+  describe 'Methods' do
+    context '#cache_key' do
+      it 'return the default value with the locale' do
+        expect(@category.cache_key).to match(/-#{Globalize.locale.to_s}\z/)
+      end
+    end
   end
 
-  it 'Translate category to fr' do
-    @category.update(name: 'Category FR', locale: :fr)
-    I18n.locale = :fr
-    expect(@category.name).to eq('Category FR')
-    I18n.locale = :en
-    expect(@category.name).to eq('00 Category')
-  end
-
-  it 'Common and scientific name validation' do
-    @category = Category.new(name: '')
-
-    @category.valid?
-    expect { @category.save! }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank")
-  end
-
-  it 'Fetch all categories' do
-    expect(Category.fetch_all(nil).count).to eq(2)
+  describe 'Scopes' do
+    context '#by_name_asc' do
+      it 'Order by name asc' do
+        expect(Category.by_name_asc.first.name).to eq('00 Category')
+      end
+    end
   end
 end
