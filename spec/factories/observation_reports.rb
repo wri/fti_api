c0a7f7a7f43f429403924a3ff39f2d47f0c17467
Jesús González <jesus198927@gmail.com>
2019-07-08 16:53:20 +0200

    Add test for observation_report model

---
+++ spec/factories/observation_reports.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :observation_report do
+    sequence(:title) { |n| "ObservationReportTitle#{n}" }
+    publication_date { DateTime.current }
+    attachment { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'files', 'image.png')) }
+
+    after(:build) do |random_observation_report|
+      random_observation_report.user ||= FactoryGirl.create(:user)
+    end
+  end
+end
