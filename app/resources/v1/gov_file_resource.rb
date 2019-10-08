34b845865c8ccc9c62fcb4dd86aca9273f6f14ac
Tiago Santos <santos.tiago@gmail.com>
2019-10-08 18:24:27 +0200

    Fixed routes for gov files

--- app/resources/v1/gov_file_resource.rb
+++ app/resources/v1/gov_file_resource.rb
@@ -3,20 +3,9 @@
 module V1
   class GovFileResource < JSONAPI::Resource
     caching
-    attributes :attachment
+    attributes :attachment, :gov_document_id
 
     has_one :gov_document
-    has_one :user
-
-    before_create :set_user_id
-
-    def set_user_id
-      if context[:current_user].present?
-        @model.user_id = context[:current_user].id
-        @model.uploaded_by = :operator
-      end
-    end
-
 
     def custom_links(_)
       { self: nil }
@@ -24,10 +13,6 @@ module V1
 
 
     private
-
-    def belongs_to_user
-      context[:current_user]&.is_operator?(@model.operator_document.operator_id)
-    end
 
     # Caching conditions
     def self.attribute_caching_context(context)
