ede2be8e11e459d600cda252606aeca803dc0327
dimazab <dimazabolotny1993@gmail.com>
2020-01-03 15:05:47 +0200

    Removed comment for debugger

--- app/controllers/v1/observers_controller.rb
+++ app/controllers/v1/observers_controller.rb
@@ -10,7 +10,6 @@ module V1
 
     def update
       # When sending the logo empty, it deletes it
-      # byebug
       if params.dig('data', 'attributes', 'logo') == ""
         params['data']['attributes']['delete-logo'] = '1'
       end
