136e5ec08ea77b2b88025bb9bc6988c7dc94d2a7
dimazab <dimazabolotny1993@gmail.com>
2020-02-10 13:39:34 +0200

    Fixed FileDataImport for eager loading.

---
+++ lib/file_data_import.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative 'file_data_import/parser/csv'
+require_relative 'file_data_import/record'
+require_relative 'file_data_import/belongs_to_association'
+
+module FileDataImport
+  
+end
