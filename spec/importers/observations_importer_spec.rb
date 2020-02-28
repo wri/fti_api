0ad7d92632ea5bcdafdaaf944a90348d2fe6dadc
Tiago Santos <santos.tiago@gmail.com>
2020-02-28 16:23:55 +0100

    Fixed specs

--- spec/importers/observations_importer_spec.rb
+++ spec/importers/observations_importer_spec.rb
@@ -4,8 +4,8 @@ describe ObservationsImporter, type: :importer do
   let!(:country) { create(:country, iso: "DE", name: "Germany") }
 
   let(:importer_type) { :observations }
-  let(:uploaded_file) { fixture_file_upload("#{importer_type}/import_data.csv", Mime[:csv].to_s) }
-  let(:results) { fixture_file_upload("#{importer_type}/results.json", Mime[:json].to_s).read }
+  let(:uploaded_file) { fixture_file_upload("data_import/#{importer_type}/import_data.csv", Mime[:csv].to_s) }
+  let(:results) { fixture_file_upload("data_import/#{importer_type}/results.json", Mime[:json].to_s).read }
   let(:importer) { FileDataImport::BaseImporter.build(importer_type, uploaded_file) }
 
   context "CSV" do
