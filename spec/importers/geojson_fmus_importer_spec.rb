4da5c98821e732722a5ac1116ac6c17881f5fb1f
Tiago Santos <santos.tiago@gmail.com>
2020-03-02 17:53:16 +0100

    Fixing tests

--- spec/importers/geojson_fmus_importer_spec.rb
+++ spec/importers/geojson_fmus_importer_spec.rb
@@ -5,8 +5,8 @@ describe GeojsonFmusImporter, type: :importer do
   let!(:fmu) { create(:fmu, id: 100187, name: "asdf", forest_type: 2) }
 
   let(:importer_type) { "geojson_fmus" }
-  let(:uploaded_file) { fixture_file_upload("#{importer_type}/import_data.zip") }
-  let(:results) { fixture_file_upload("#{importer_type}/results.json", Mime[:json].to_s).read }
+  let(:uploaded_file) { fixture_file_upload("data_import/#{importer_type}/import_data.zip") }
+  let(:results) { fixture_file_upload("data_import/#{importer_type}/results.json", Mime[:json].to_s).read }
   let(:importer) { FileDataImport::BaseImporter.build(importer_type, uploaded_file) }
 
   context "ZIP with Esri shape files" do
--- spec/importers/observations_importer_spec.rb
+++ spec/importers/observations_importer_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe ObservationsImporter, type: :importer do
   let!(:country) { create(:country, iso: "DE", name: "Germany") }
 
-  let(:importer_type) { :observations }
+  let(:importer_type) { 'observations' }
   let(:uploaded_file) { fixture_file_upload("data_import/#{importer_type}/import_data.csv", Mime[:csv].to_s) }
   let(:results) { fixture_file_upload("data_import/#{importer_type}/results.json", Mime[:json].to_s).read }
   let(:importer) { FileDataImport::BaseImporter.build(importer_type, uploaded_file) }
