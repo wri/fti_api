6877bc7e18db11a940ecb9e604f41522093bcb14
Tiago Santos <santos.tiago@gmail.com>
2020-03-05 17:55:48 +0100

    Mid commit

--- spec/acceptance/countries_spec.rb
+++ spec/acceptance/countries_spec.rb
@@ -1,18 +1,22 @@
 require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
-resource 'Fmus' do
-  explanation "FMUs resource"
+resource 'Countries' do
+  explanation "Countries resource"
+
+  admin = User.joins(:user_permission).where(user_permissions: { user_role: 'admin' }).first || FactoryBot.create(:admin)
+  admin_token = admin.api_key.access_token
 
   web_user = User.find_by(name: 'Web user')
   web_token = web_user.api_key.access_token
 
   header "Content-Type", "application/vnd.api+json"
+  header 'Authorization', "Bearer #{admin_token}"
 
   authentication :apiKey, "Bearer #{web_token}" , name: 'OTP-API-KEY'
 
-  get "/fmus" do
-    example "Listing fmus" do
+  get "/countries" do
+    example "Listing countries" do
       do_request
 
       expect(status).to eq 200
--- spec/acceptance/operators_spec.rb
+++ spec/acceptance/operators_spec.rb
@@ -1,18 +1,22 @@
 require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
-resource 'Fmus' do
-  explanation "FMUs resource"
+resource 'Operators' do
+  explanation "Operators resource"
+
+  admin = User.joins(:user_permission).where(user_permissions: { user_role: 'admin' }).first || FactoryBot.create(:admin)
+  admin_token = admin.api_key.access_token
 
   web_user = User.find_by(name: 'Web user')
   web_token = web_user.api_key.access_token
 
   header "Content-Type", "application/vnd.api+json"
+  header 'Authorization', "Bearer #{admin_token}"
 
   authentication :apiKey, "Bearer #{web_token}" , name: 'OTP-API-KEY'
 
-  get "/fmus" do
-    example "Listing fmus" do
+  get "/operators" do
+    example "Listing operators" do
       do_request
 
       expect(status).to eq 200
