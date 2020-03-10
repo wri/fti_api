ae234b27843afa60a0f6d7be79e355c45f1f0daf
Tiago Santos <santos.tiago@gmail.com>
2020-03-10 17:26:21 +0100

    Fixed the vector-tiles query and the coordinates order

--- app/models/fmu.rb
+++ app/models/fmu.rb
@@ -81,7 +81,7 @@ class Fmu < ApplicationRecord
     end
 
     # Returns a vector tile for the X,Y,Z provided
-    def vector_tiles(param_x, param_y, param_z)
+    def vector_tiles(param_z, param_x, param_y)
       begin
         x, y, z = Integer(param_x), Integer(param_y), Integer(param_z)
       rescue ArgumentError, TypeError
@@ -91,9 +91,9 @@ class Fmu < ApplicationRecord
       query =
           <<~SQL
             SELECT ST_ASMVT(tile.*, 'layer0', 4096, 'mvtgeometry', 'id') as tile
-             FROM (SELECT id, properties, ST_AsMVTGeom(the_geom_webmercator, ST_TileEnvelope(1,1,1), 4096, 256, true) AS mvtgeometry
+             FROM (SELECT id, properties, ST_AsMVTGeom(the_geom_webmercator, ST_TileEnvelope(#{z},#{x},#{y}), 4096, 256, true) AS mvtgeometry
                                       FROM (select *, st_transform(geometry, 3857) as the_geom_webmercator from fmus) as data 
-                                    WHERE ST_AsMVTGeom(the_geom_webmercator, ST_TileEnvelope(#{x},#{y},#{z}),4096,0,true) IS NOT NULL) AS tile;
+                                    WHERE ST_AsMVTGeom(the_geom_webmercator, ST_TileEnvelope(#{z},#{x},#{y}),4096,0,true) IS NOT NULL) AS tile;
           SQL
 
       tile = ActiveRecord::Base.connection.execute query
