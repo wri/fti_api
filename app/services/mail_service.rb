5b2f26537605d1a409bdead8963840bffd4dfcf8
Tiago Santos <santos.tiago@gmail.com>
2020-05-26 10:00:23 +0200

    Changed the observers notification to send an email in english and french together

--- app/services/mail_service.rb
+++ app/services/mail_service.rb
@@ -79,6 +79,7 @@ TXT
 
     text = t('backend.mail_service.observer_status_changed.text',
              id: observation.id, observer: observer.name, status: observation.validation_status,
+             status_fr: I18n.t("activerecord.enums.observation.statuses.#{observation.validation_status}", locale: :fr),
              date: observation.publication_date, infractor_text: infractor_text,
              infraction: observation.subcategory&.name)
     observer.users.each do |user|
