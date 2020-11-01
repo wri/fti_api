c320a2c6fc06baa7c6e053e23a85657e8f86dcab
Tiago Santos <santos.tiago@gmail.com>
2020-11-01 18:02:39 +0100

    Added test to the DocumentHistory model

--- spec/models/operator_document_history_spec.rb
+++ spec/models/operator_document_history_spec.rb
@@ -26,5 +26,54 @@
 require 'rails_helper'
 
 RSpec.describe OperatorDocumentHistory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Existence' do
+    let (:od) { FactoryBot.create(:operator_document_country) }
+    context 'Creating an OperatorDocument' do
+      it 'Adds an OperatorDocumentHistory' do
+        odh = OperatorDocumentHistory.find_by(operator_document_id: od.id)
+        expect(odh).not_to be_nil
+        expect(odh.type).to eql(od.type  + 'History')
+        expect(odh.expire_date).to eql(od.expire_date)
+        expect(odh.start_date).to eql(od.start_date)
+        expect(odh.uploaded_by).to eql(od.uploaded_by)
+        expect(odh.reason).to eql(od.reason)
+        expect(odh.note).to eql(od.note)
+        expect(odh.response_date).to eql(od.response_date)
+        expect(odh.public).to eql(od.public)
+        expect(odh.source).to eql(od.source)
+        expect(odh.source_info).to eql(od.source_info)
+        expect(odh.fmu_id).to eql(od.fmu_id)
+        expect(odh.document_file_id).to eql(od.document_file_id)
+        expect(odh.created_at.to_i).to eql(od.created_at.to_i)
+        expect(odh.updated_at.to_i).to eql(od.updated_at.to_i)
+        expect(odh.operator_id).to eql(od.operator_id)
+        expect(odh.user_id).to eql(od.user_id)
+        expect(odh.required_operator_document_id).to eql(od.required_operator_document_id)
+      end
+    end
+    context 'Updating an OperatorDocument' do
+      it 'Adds a new version of the OperatorDocumentHistory' do
+        od.update(note: 'new note')
+        od.reload
+        odhs = OperatorDocumentHistory.where(operator_document_id: od.id).order({updated_at: :desc})
+        expect(odhs.count).to eql(2)
+        expect(odhs.first.note).to eql('new note')
+        expect(odhs.first.updated_at.to_i).to eql(od.updated_at.to_i)
+        expect(odhs.last.note).to be_nil
+      end
+    end
+    context 'Deletes an OperatorDocument' do
+      it 'Adds a new version of OperatorDocumentHistory and keeps the file' do
+        document_file = od.document_file
+        od.destroy
+        od.reload
+        odhs = OperatorDocumentHistory.where(operator_document_id: od.id).order({updated_at: :desc})
+        expect(odhs.count).to eql(2)
+        expect(odhs.first.status).to eql('doc_not_provided')
+        expect(odhs.last.document_file_id).to eql(document_file.id)
+        expect(document_file.attachment).not_to be_nil
+      end
+    end
+  end
 end
