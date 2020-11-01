6218e83310c45720450680247e7394008e2d0e5c
Tiago Santos <santos.tiago@gmail.com>
2020-11-01 20:46:11 +0100

    Fixed the function to search a document in time

--- app/models/operator_document_history.rb
+++ app/models/operator_document_history.rb
@@ -38,4 +38,18 @@ class OperatorDocumentHistory < ApplicationRecord
   enum uploaded_by: { operator: 1, monitor: 2, admin: 3, other: 4 }
   enum source: { company: 1, forest_atlas: 2, other_source: 3 }
 
+  # Returns the operator documents for an operator at a point in time
+  # @param String operator_id The operator id
+  # @param String date the date at which to fetch the state
+  def self.from_operator_at_date(operator_id, date)
+    query = <<~SQL
+        (select * from
+        (select row_number() over (partition by required_operator_document_id, fmu_id order by updated_at desc), *
+        from operator_document_histories
+        where operator_id = #{operator_id} AND updated_at < '#{date.to_date.to_s(:db)}') as sq
+        where sq.row_number = 1) as operator_document_histories
+    SQL
+
+    from(query)
+  end
 end
