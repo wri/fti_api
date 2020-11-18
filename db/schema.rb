c3efc334feabdecb6ab6fcec12ea9a596133f846
Tiago Santos <santos.tiago@gmail.com>
2020-11-18 15:45:54 +0100

    Required Gov Documents Groups are now delete proof

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201117145551) do
+ActiveRecord::Schema.define(version: 20201118141952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -669,6 +669,8 @@ ActiveRecord::Schema.define(version: 20201117145551) do
     t.integer  "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_required_gov_document_groups_on_deleted_at", using: :btree
   end
 
   create_table "required_gov_document_translations", force: :cascade do |t|
