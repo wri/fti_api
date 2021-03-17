db17ce6b2ee810f4d07ee341fe10e588cc4d9331
develaper <ibrahim.lachguer@gmail.com>
2021-03-17 13:27:29 +0100

    Removes doc_not_provided from records

--- app/resources/v1/operator_document_resource.rb
+++ app/resources/v1/operator_document_resource.rb
@@ -48,7 +48,7 @@ module V1
     end
 
     def self.records(options = {})
-      OperatorDocument.from_active_operators
+      OperatorDocument.provided.from_active_operators
     end
 
     def custom_links(_)
--- spec/integration/v1/operator_documents_spec.rb
+++ spec/integration/v1/operator_documents_spec.rb
@@ -16,16 +16,24 @@ module V1
             it 'returns all' do
                 get('/operator-documents?locale=en', headers: admin_headers)
                 expect(operator_documents.count).to eql(10)
-                expect(parsed_data.count).to eql(10)
+                expect(parsed_data.count).to eql(operator_documents.count)
 
             end
             it 'returns all with included' do
                 get(operator_documents_url_with_included, headers: admin_headers)
 
-                expect(operator_documents.count).to eql(10)
-                expect(parsed_data.count).to eql(10)
+                expect(parsed_data.count).to eql(operator_documents.count)
                 expect(parsed_body[:included].any?).to eql(true)
 
+            end
+            it 'returns only provided OperatorDocuments' do
+                operator_document_not_provided = FactoryBot.create(:operator_document)
+                operator_document_not_provided.status = OperatorDocument.statuses[:doc_not_provided]
+                operator_document_not_provided.save!
+                get(operator_documents_url_with_included, headers: admin_headers)
+
+                expect(parsed_data.count).to eql(operator_documents.count)
+                expect(parsed_data.find { |item| item[:id] == operator_document_not_provided.id }).to be_nil
             end
         end
     end
