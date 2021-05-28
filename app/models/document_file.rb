6992729535f9da9ea4fe1ed708c8180cda6f805d
Tomasz Subik <tsubik@gmail.com>
2021-05-28 12:26:57 +0200

    drop file_name from DocumentFile as it is redundant field

--- app/models/document_file.rb
+++ app/models/document_file.rb
@@ -6,7 +6,6 @@
 #
 #  id         :integer          not null, primary key
 #  attachment :string
-#  file_name  :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210527135115) do
+ActiveRecord::Schema.define(version: 20210528085923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -192,7 +192,6 @@ ActiveRecord::Schema.define(version: 20210527135115) do
 
   create_table "document_files", force: :cascade do |t|
     t.string   "attachment"
-    t.string   "file_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
