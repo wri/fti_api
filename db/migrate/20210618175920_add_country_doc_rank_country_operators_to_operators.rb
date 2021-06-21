b50d3058b5393ced23468228e9981c0450aff07a
Tomasz Subik <tsubik@gmail.com>
2021-06-21 13:04:20 +0200

    buggy rails version not allowing to refresh rank in migration?

--- db/migrate/20210618175920_add_country_doc_rank_country_operators_to_operators.rb
+++ db/migrate/20210618175920_add_country_doc_rank_country_operators_to_operators.rb
@@ -2,11 +2,5 @@ class AddCountryDocRankCountryOperatorsToOperators < ActiveRecord::Migration[5.0
   def change
     add_column :operators, :country_doc_rank, :integer
     add_column :operators, :country_operators, :integer
-
-    reversible do |dir|
-      dir.up do
-        RankingOperatorDocument.refresh
-      end
-    end
   end
 end
