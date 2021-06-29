d4f267f92c1f334b32c4f45b9cf77bf7a415102d
Tomasz Subik <tsubik@gmail.com>
2021-06-29 17:32:45 +0200

    ohh those sequences

--- spec/integration/v1/operator_document_filters_spec.rb
+++ spec/integration/v1/operator_document_filters_spec.rb
@@ -6,13 +6,13 @@ module V1
       before :all do
         country_1 = create(:country, name: 'Poland', iso: 'PL')
         country_2 = create(:country, name: 'Spain', iso: 'ES')
-        operator_1 = create(:operator, country: country_1, fa_id: 'fa_id1')
-        operator_2 = create(:operator, country: country_1, fa_id: 'fa_id2')
-        operator_3 = create(:operator, country: country_2, fa_id: 'fa_id3')
+        operator_1 = create(:operator, country: country_1, fa_id: 'fa_id1', name: 'operator 1')
+        operator_2 = create(:operator, country: country_1, fa_id: 'fa_id2', name: 'operator 2')
+        operator_3 = create(:operator, country: country_2, fa_id: 'fa_id3', name: 'operator 3')
 
-        fmu_1 = create(:fmu, country: country_1, forest_type: 1)
-        fmu_2 = create(:fmu, country: country_1, forest_type: 2)
-        fmu_3 = create(:fmu, country: country_2, forest_type: 1)
+        fmu_1 = create(:fmu, country: country_1, forest_type: 1, name: 'fmu 1')
+        fmu_2 = create(:fmu, country: country_1, forest_type: 2, name: 'fmu 2')
+        fmu_3 = create(:fmu, country: country_2, forest_type: 1, name: 'fmu 3')
 
         create(:fmu_operator, operator: operator_1, fmu: fmu_1)
         create(:fmu_operator, operator: operator_2, fmu: fmu_2)
