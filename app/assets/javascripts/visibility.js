432209d87c8cef0bc2996767442424bd09608598
Tomasz Subik <tsubik@gmail.com>
2021-07-29 13:35:41 +0200

    2 way sync chart with column visible

--- app/assets/javascripts/visibility.js
+++ app/assets/javascripts/visibility.js
@@ -1,9 +1,34 @@
 $(document).ready(function() {
+  function updateChart(elemId, checked) {
+    const chart = Chartkick.charts['chart-1'].chart;
+    const dataset = chart.data.datasets.find(
+      x => x.label.replace('&', '').replace(' ', '').toLowerCase() === elemId.replace('_', '')
+    );
+    if (dataset) {
+      dataset.hidden = !checked;
+      chart.update();
+    }
+  }
+
+  setTimeout(function() {
+    const chart = Chartkick.charts['chart-1'].chart;
+    const oldOnClick = chart.options.legend.onClick;
+    const newLegendClickHandler = function (e, legendItem) {
+      const id = legendItem.text.replace(' & ', ' ').replace(' ', '_').toLowerCase();
+      const column = $(`.col-${id}`);
+      $('#' + id).prop('checked', legendItem.hidden);
+      column.toggleClass('hide', !legendItem.hidden);
+      oldOnClick.apply(this, [e, legendItem]);
+    };
+    chart.options.legend.onClick = newLegendClickHandler;
+  }, 100)
+
   $(".observation-checkbox").on('change', function(event) {
     const elem = event.target;
     const column = $(`.col-${elem.id}`);
     column.toggleClass('hide', !elem.checked);
     localStorage.setItem(elem.id, elem.checked);
+    updateChart(elem.id, elem.checked);
   });
 
   if ($('.observation-attributes').length > 0) {
@@ -18,7 +43,9 @@ $(document).ready(function() {
         }
       }
       const column = $(`.col-${id}`);
-      column.toggleClass('hide', !$('#' + id).prop('checked'));
+      const checked = $('#' + id).prop('checked');
+      column.toggleClass('hide', !checked);
+      updateChart(id, checked);
     })
   }
 });
