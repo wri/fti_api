2086872b8af487705a7f251106fcbdcf9105974e
Tomasz Subik <tsubik@gmail.com>
2021-08-04 10:07:29 +0200

    add category filter, small refactor, default date from 1 year ago

--- app/admin/observations_dashboard.rb
+++ app/admin/observations_dashboard.rb
@@ -11,9 +11,9 @@ ActiveAdmin.register ObservationStatistic, as: 'Observations Dashboard' do
   filter :by_country, label: 'Country', as: :select, collection: [['All Countries', 'null']] + Country.where(id: Observation.pluck(:country_id)).map { |c| [c.name, c.id] }
   filter :operator, label: 'Operator', as: :select, collection: Operator.where(id: Observation.pluck(:operator_id))
   filter :fmu_forest_type_eq, label: 'Forest Type', as: :select, collection: Fmu::FOREST_TYPES.map { |ft| [ft.last[:label], ft.last[:index]] }
-  # filter :category,
-  #   label: 'Category', as: :select,
-  #   collection: -> { Category.with_translations(I18n.locale).order('category_translations.name') }
+  filter :category,
+    label: 'Category', as: :select,
+    collection: -> { Category.with_translations(I18n.locale).order('category_translations.name') }
   filter :subcategory,
     label: 'Subcategory', as: :select,
     collection: -> { Subcategory.with_translations(I18n.locale).order('subcategory_translations.name') }
@@ -58,6 +58,13 @@ ActiveAdmin.register ObservationStatistic, as: 'Observations Dashboard' do
         'All Statuses'
       else
         r.validation_status
+      end
+    end
+    column :category do |r|
+      if r.category.nil?
+        'All Categories'
+      else
+        link_to r.category.name, admin_category_path(r.category)
       end
     end
     column :subcategory do |r|
@@ -108,6 +115,13 @@ ActiveAdmin.register ObservationStatistic, as: 'Observations Dashboard' do
         r.validation_status
       end
     end
+    column :category do |r|
+      if r.category.nil?
+        'All Categories'
+      else
+        r.category.name
+      end
+    end
     column :subcategory do |r|
       if r.subcategory.nil?
         'All Subcategories'
@@ -122,96 +136,19 @@ ActiveAdmin.register ObservationStatistic, as: 'Observations Dashboard' do
   controller do
     skip_before_action :restore_search_filters
     skip_after_action :save_search_filters
+    before_action :set_default_filters
+
+    def set_default_filters
+      params[:q] ||= {}
+      params[:q][:date_gteq] = 1.year.ago if params.dig(:q, :date_gteq).blank?
+    end
 
     def find_collection(options = {})
-      date_from = params.dig(:q, :date_gteq) || Observation.order(:created_at).first.created_at.to_date
-      date_to = params.dig(:q, :date_lteq) || Date.today.to_date
-      country_id = params.dig(:q, :by_country)
-      operator_id = params.dig(:q, :operator_id_eq)
-      subcategory_id = params.dig(:q, :subcategory_id_eq)
-      validation_status = params.dig(:q, :validation_status_eq)
-      severity_level = params.dig(:q, :severity_level_eq)
-      forest_type = params.dig(:q, :fmu_forest_type_eq)
-
-      filters = [
-        country_id.nil? || country_id == 'null' ? 'country_id is not null' : "country_id = #{country_id}",
-        operator_id.nil? ? nil : "operator_id = #{operator_id}",
-        validation_status.nil? ? nil : "validation_status = #{validation_status}",
-        forest_type.nil? ? nil : "fmu_forest_type = #{forest_type}",
-        severity_level.nil? ? nil : "severity_level = #{severity_level}",
-        subcategory_id.nil? ? nil : "subcategory_id = #{subcategory_id}"
-      ].compact.join(' AND ')
-      select = [
-        operator_id.nil? ? nil : "#{operator_id} as operator_id",
-        validation_status.nil? ? nil : "#{validation_status} as validation_status",
-        severity_level.nil? ? nil : "#{severity_level} as severity_level",
-        subcategory_id.nil? ? nil : "#{subcategory_id} as subcategory_id",
-        forest_type.nil? ? nil : "#{forest_type} as forest_type"
-      ].compact.join(',')
-
-      sql = <<~SQL
-        with dates as (
-          SELECT date_trunc('day', dd)::date as date
-          FROM generate_series
-              ( '#{date_from.to_s(:db)}'::timestamp
-              , '#{date_to.to_s(:db)}'::timestamp
-              , '1 day'::interval) dd
-        ),
-        grouped as (
-          select
-            date,
-            country_id,
-            count(*) as total_count
-          from
-          dates
-          left join lateral
-            (
-              select * from (
-                select row_number() over (partition by observation_id order by observation_updated_at desc), *
-                  from observation_histories
-                where observation_updated_at <= dates.date
-              ) as sq
-              where sq.row_number = 1
-            ) as observations_by_date on 1=1
-          #{filters.present? ? 'where ' + filters : ''}
-          group by date, rollup(country_id)
-        )
-        select
-          date,
-          country_id,
-          #{select.present? ? select + ',' : ''}
-          total_count
-        from (
-          select
-            *,
-            LAG(total_count,1) OVER (
-              partition by country_id
-              ORDER BY date
-            ) prev_total
-            from grouped
-        ) as total_c
-        where
-          (prev_total is null or prev_total != total_count or date = '#{date_to.to_s(:db)}}' or date = '#{date_from.to_s(:db)}}')
-          AND (#{country_id.nil? || country_id == 'null' ? '1=1' : 'country_id is not null'})
-          AND (#{country_id == 'null' ? 'country_id is null' : '1=1'})
-        order by date desc
-      SQL
-
-      collection = ActiveRecord::Base.connection.execute(sql).to_a.map do |row|
-        ObservationStatistic.new(
-          date: row['date'],
-          country_id: row['country_id'],
-          total_count: row['total_count'],
-          operator_id: row['operator_id'],
-          subcategory_id: row['subcategory_id'],
-          validation_status: row['validation_status'],
-          fmu_forest_type: row['forest_type'],
-          severity_level: row['severity_level']
-        )
-      end
-
+      collection = ObservationStatistic.query_dashboard_report(params[:q] || {})
+      # keep the ransack to maintain filters in active admin
       @search = ObservationStatistic.search(params[:q] || {})
-      Kaminari.paginate_array(collection).page(1).per(100000)
+      # collection must be paged otherwise aa is complaining
+      Kaminari.paginate_array(collection).page(1).per(10000)
     end
   end
 end
--- app/models/observation_statistic.rb
+++ app/models/observation_statistic.rb
@@ -24,10 +24,12 @@ class ObservationStatistic < ApplicationRecord
   belongs_to :subcategory, optional: true
   belongs_to :operator, optional: true
 
-  enum validation_status: { "Created" => 0, "Ready for QC" => 1, "QC in progress" => 2, "Approved" => 3,
-                           "Rejected" => 4, "Needs revision" => 5, "Ready for publication" => 6,
-                           "Published (no comments)" => 7, "Published (not modified)" => 8,
-                           "Published (modified)" => 9 }
+  enum validation_status: {
+    "Created" => 0, "Ready for QC" => 1, "QC in progress" => 2, "Approved" => 3,
+    "Rejected" => 4, "Needs revision" => 5, "Ready for publication" => 6,
+    "Published (no comments)" => 7, "Published (not modified)" => 8,
+    "Published (modified)" => 9
+  }
 
   validates_presence_of :date
 
@@ -39,6 +41,98 @@ class ObservationStatistic < ApplicationRecord
   # just to hack around active admin, does not have to filter by country
   def self.by_country(country_id = nil)
     all
+  end
+
+  def self.query_dashboard_report(search = {})
+    date_from = (search[:date_gteq] || Observation.order(:created_at).first.created_at).to_date.to_s(:db)
+    date_to = (search[:date_lteq] || Date.today).to_date.to_s(:db)
+    country_id = search[:by_country]
+    operator_id = search[:operator_id_eq]
+    subcategory_id = search[:subcategory_id_eq]
+    category_id = search[:category_id_eq]
+    validation_status = search[:validation_status_eq]
+    severity_level = search[:severity_level_eq]
+    forest_type = search[:fmu_forest_type_eq]
+
+    filters = [
+      country_id.nil? || country_id == 'null' ? 'country_id is not null' : "country_id = #{country_id}",
+      operator_id.nil? ? nil : "operator_id = #{operator_id}",
+      validation_status.nil? ? nil : "validation_status = #{validation_status}",
+      forest_type.nil? ? nil : "fmu_forest_type = #{forest_type}",
+      severity_level.nil? ? nil : "severity_level = #{severity_level}",
+      subcategory_id.nil? ? nil : "subcategory_id = #{subcategory_id}",
+      category_id.nil? ? nil : "category_id = #{category_id}"
+    ].compact.join(' AND ')
+    select = [
+      operator_id.nil? ? nil : "#{operator_id} as operator_id",
+      validation_status.nil? ? nil : "#{validation_status} as validation_status",
+      severity_level.nil? ? nil : "#{severity_level} as severity_level",
+      subcategory_id.nil? ? nil : "#{subcategory_id} as subcategory_id",
+      category_id.nil? ? nil : "#{category_id} as category_id",
+      forest_type.nil? ? nil : "#{forest_type} as fmu_forest_type"
+    ].compact.join(',')
+
+    sql = <<~SQL
+      with dates as (
+        SELECT date_trunc('day', dd)::date as date
+        FROM generate_series
+            ( '#{date_from}'::timestamp
+            , '#{date_to}'::timestamp
+            , '1 day'::interval) dd
+      ),
+      grouped as (
+        select
+          date,
+          country_id,
+          count(*) as total_count
+        from
+        dates
+        left join lateral
+          (
+            select * from (
+              select row_number() over (partition by observation_id order by observation_updated_at desc), *
+                from observation_histories
+              where observation_updated_at <= dates.date
+            ) as sq
+            where sq.row_number = 1
+          ) as observations_by_date on 1=1
+        #{filters.present? ? 'where ' + filters : ''}
+        group by date, rollup(country_id)
+      )
+      select
+        date,
+        country_id,
+        #{select.present? ? select + ',' : ''}
+        total_count
+      from (
+        select
+          *,
+          LAG(total_count,1) OVER (
+            partition by country_id
+            ORDER BY date
+          ) prev_total
+          from grouped
+      ) as total_c
+      where
+        (prev_total is null or prev_total != total_count or date = '#{date_to}' or date = '#{date_from}')
+        AND (#{country_id.nil? || country_id == 'null' ? '1=1' : 'country_id is not null'})
+        AND (#{country_id == 'null' ? 'country_id is null' : '1=1'})
+      order by date desc
+    SQL
+
+    ActiveRecord::Base.connection.execute(sql).to_a.map do |row|
+      ObservationStatistic.new(
+        date: row['date'],
+        country_id: row['country_id'],
+        total_count: row['total_count'],
+        operator_id: row['operator_id'],
+        category_id: row['category_id'],
+        subcategory_id: row['subcategory_id'],
+        validation_status: row['validation_status'],
+        fmu_forest_type: row['fmu_forest_type'],
+        severity_level: row['severity_level']
+      )
+    end
   end
 
   def country_name
