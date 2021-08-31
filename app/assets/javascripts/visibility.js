86c28e87ffcb4ac380d7ee4cb62f7d60808db46f
Tomasz Subik <tsubik@gmail.com>
2021-08-31 13:14:01 +0200

    workaround to fix chart width after render

--- app/assets/javascripts/visibility.js
+++ app/assets/javascripts/visibility.js
@@ -11,18 +11,22 @@ $(document).ready(function() {
   }
 
   setTimeout(function() {
+    Chartkick.charts['chart-1'].redraw();
+    setTimeout(updateColumnVisiblity, 100);
+  }, 50);
+
+  setTimeout(function() {
     const chart = Chartkick.charts['chart-1'].chart;
     const oldOnClick = chart.options.legend.onClick;
     const newLegendClickHandler = function (e, legendItem) {
       const id = legendItem.text.replaceAll(' & ', ' ').replaceAll(' ', '_').toLowerCase();
-      console.log('lookinf for id', id)
       const column = $(`.col-${id}`);
       $('#' + id).prop('checked', legendItem.hidden);
       column.toggleClass('hide', !legendItem.hidden);
       oldOnClick.apply(this, [e, legendItem]);
     };
     chart.options.legend.onClick = newLegendClickHandler;
-  }, 100)
+  }, 300)
 
   $(".observation-checkbox").on('change', function(event) {
     const elem = event.target;
@@ -32,25 +36,29 @@ $(document).ready(function() {
     updateChart(elem.id, elem.checked);
   });
 
-  if ($('.observation-attributes').length > 0) {
-    const saveToLocalStorage = $('.observation-attributes').data('saveToLocalStorage');
+  function updateColumnVisiblity() {
+    if ($('.observation-attributes').length > 0) {
+      const saveToLocalStorage = $('.observation-attributes').data('saveToLocalStorage');
 
-    $('.observation-attributes').children().each((_idx, elem) => {
-      const id = elem.id.split('-')[1];
-      if (saveToLocalStorage) {
-        const storage = localStorage.getItem(id);
-        if (storage !== null) {
-          if(storage === "true") {
-            $('#' + id).prop("checked", true);
-          } else {
-            $('#' + id).prop("checked", false);
+      $('.observation-attributes').children().each((_idx, elem) => {
+        const id = elem.id.split('-')[1];
+        if (saveToLocalStorage) {
+          const storage = localStorage.getItem(id);
+          if (storage !== null) {
+            if(storage === "true") {
+              $('#' + id).prop("checked", true);
+            } else {
+              $('#' + id).prop("checked", false);
+            }
           }
         }
-      }
-      const column = $(`.col-${id}`);
-      const checked = $('#' + id).prop('checked');
-      column.toggleClass('hide', !checked);
-      updateChart(id, checked);
-    })
+        const column = $(`.col-${id}`);
+        const checked = $('#' + id).prop('checked');
+        column.toggleClass('hide', !checked);
+        updateChart(id, checked);
+      })
+    }
   }
+
+  updateColumnVisiblity();
 });
