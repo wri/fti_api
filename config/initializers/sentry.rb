f58b1247f1e84c7a8e3b2ef512ff1625166efe72
Tomasz Subik <tsubik@gmail.com>
2022-06-30 18:45:02 +0200

    fix sentry not logging API issues

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,7 +7,7 @@ Sentry.init do |config|
   # Set tracesSampleRate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production
-  config.traces_sample_rate = 1.0
+  config.traces_sample_rate = 0.5
   # or
   # config.traces_sampler = lambda do |context|
   #   true
