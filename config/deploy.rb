c036226ad6bb77ece7074f4be6a3dfda8df0cf03
Tomasz Subik <tsubik@gmail.com>
2022-08-01 12:28:07 +0200

    update capistrano lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # config valid only for current version of Capistrano
-lock '3.12.0'
+lock '~> 3.12'
 
 set :application, 'OtpAPI'
 set :repo_url, 'git@github.com:Vizzuality/fti_api.git'
