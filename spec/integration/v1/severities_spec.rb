f20bb90847d11936f3bdc7a34d01e8a02b3568d0
Tomasz Subik <tsubik@gmail.com>
2022-08-17 17:56:22 +0200

    severities, subcategories specs

---
+++ spec/integration/v1/severities_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+module V1
+  describe 'Severities', type: :request do
+    it_behaves_like "jsonapi-resources", Severity, {
+      show: {
+        success_roles: %i[admin],
+      },
+      pagination: {
+        success_roles: %i[admin]
+      },
+      sort: {
+        success_roles: %i[admin],
+        attribute: :details,
+        sequence: -> (i) { "Details #{i}" }
+      },
+      filter: {
+        success_roles: %i[admin],
+        filters: [
+          { attributes: { subcategory_id: FactoryBot.create(:subcategory).id } }
+        ]
+      }
+    }
+  end
+end
