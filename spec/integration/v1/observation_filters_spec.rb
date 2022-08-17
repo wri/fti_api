9b79331ee20c8e3c7aef4e35180c3c8332233e50
Tomasz Subik <tsubik@gmail.com>
2022-08-17 19:41:23 +0200

    more missing specs, maybe csv could be removed

--- spec/integration/v1/observation_filters_spec.rb
+++ spec/integration/v1/observation_filters_spec.rb
@@ -9,5 +9,37 @@ module V1
         expect(status).to eql(200)
       end
     end
+
+    describe 'csv' do
+      let(:country) { create(:country, name: 'Country') }
+      let(:country2) { create(:country, name: 'Country 2') }
+
+      before do
+        create_list(:observation, 3, country: country)
+        create_list(:observation, 2, country: country2)
+      end
+
+      it 'returns csv file' do
+        get '/observations-csv', headers: non_api_webuser_headers
+
+        csv_rows = CSV.parse(response.body)
+
+        expect(response.header['Content-Type']).to include('text/csv')
+        expect(status).to eql(200)
+        expect(csv_rows.count).to eq(3 + 2 + 1) # +1 because of header
+      end
+
+      context 'filters' do
+        it 'returns filtered csv' do
+          get "/observations-csv?filter[country_id]=#{country2.id}", headers: non_api_webuser_headers
+
+          csv_rows = CSV.parse(response.body)
+
+          expect(response.header['Content-Type']).to include('text/csv')
+          expect(status).to eql(200)
+          expect(csv_rows.count).to eq(2 + 1) # +1 because of header
+        end
+      end
+    end
   end
 end
