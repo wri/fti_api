aa4f953db83652459da4847feeb279138adb84a0
Tomasz Subik <tsubik@gmail.com>
2022-11-04 18:01:52 +0100

    fix specs

--- app/models/concerns/activable.rb
+++ app/models/concerns/activable.rb
@@ -8,30 +8,30 @@ module Activable
 
     scope :filter_actives,   -> { where(is_active: true)  }
     scope :filter_inactives, -> { where(is_active: false) }
+  end
 
-    def activate
-      update! is_active: true
-    end
+  def activate
+    update! is_active: true
+  end
 
-    def deactivate
-      update! is_active: false
-    end
+  def deactivate
+    update! is_active: false
+  end
 
-    def deactivated?
-      !is_active?
-    end
+  def deactivated?
+    !is_active?
+  end
 
-    def activated?
-      is_active?
-    end
+  def activated?
+    is_active?
+  end
 
-    def set_deactivated_at
-      self.deactivated_at = Time.now if attributes.key?('deactivated_at') && is_active_changed? && deactivated?
-    end
+  def set_deactivated_at
+    self.deactivated_at = Time.now if attributes.key?('deactivated_at') && is_active_changed? && deactivated?
+  end
 
-    def status
-      is_active? ? 'activated' : 'deactivated'
-    end
+  def status
+    is_active? ? 'activated' : 'deactivated'
   end
 
   class_methods do
