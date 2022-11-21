7f1cf8c19e11a9ec1e24df4cccee6494688726b7
Tomasz Subik <tsubik@gmail.com>
2022-11-21 11:50:17 +0100

    add empty blocks back to jsonapi routes with comment

--- config/routes.rb
+++ config/routes.rb
@@ -28,43 +28,47 @@ Rails.application.routes.draw do
 
     scope '(:locale)', locale: /en|fr/ do
       # Resources
-      jsonapi_resources :about_page_entries, only: [:index, :show]
-      jsonapi_resources :categories, only: [:index, :show]
-      jsonapi_resources :countries, only: [:index, :show]
-      jsonapi_resources :country_links, only: [:index, :show]
-      jsonapi_resources :country_vpas, only: [:index, :show]
-      jsonapi_resources :donors, only: [:index, :show]
-      jsonapi_resources :faqs, only: [:index, :show]
-      jsonapi_resources :governments
-      jsonapi_resources :gov_documents
-      jsonapi_resources :gov_files, only: [:create, :destroy]
-      jsonapi_resources :how_tos, only: [:index, :show]
-      jsonapi_resources :laws
+      # keep empty blocks to not generate relationships routes
+      # https://jsonapi-resources.com/v0.9/guide/routing.html#Nested-Routes
+      # Big plus of generating relationships routes is that it validates configured
+      # relationships, but it's better to keep API smaller
+      jsonapi_resources :about_page_entries, only: [:index, :show] do; end
+      jsonapi_resources :categories, only: [:index, :show] do; end
+      jsonapi_resources :countries, only: [:index, :show] do; end
+      jsonapi_resources :country_links, only: [:index, :show] do; end
+      jsonapi_resources :country_vpas, only: [:index, :show] do; end
+      jsonapi_resources :donors, only: [:index, :show] do; end
+      jsonapi_resources :faqs, only: [:index, :show] do; end
+      jsonapi_resources :governments do; end
+      jsonapi_resources :gov_documents do; end
+      jsonapi_resources :gov_files, only: [:create, :destroy] do; end
+      jsonapi_resources :how_tos, only: [:index, :show] do; end
+      jsonapi_resources :laws do; end
       jsonapi_resources :notifications, only: [:index] do
         put :dismiss, on: :member
       end
-      jsonapi_resources :observations
-      jsonapi_resources :observation_documents
-      jsonapi_resources :observation_reports
-      jsonapi_resources :observers, only: [:index, :show]
-      jsonapi_resources :operators
-      jsonapi_resources :operator_documents, except: [:create, :update]
-      jsonapi_resources :operator_document_annexes
-      jsonapi_resources :operator_document_countries, except: [:create]
-      jsonapi_resources :operator_document_fmus, except: [:create]
-      jsonapi_resources :operator_document_histories
-      jsonapi_resources :partners, only: [:index, :show]
-      jsonapi_resources :required_gov_documents
-      jsonapi_resources :required_operator_documents
-      jsonapi_resources :required_operator_document_groups
-      jsonapi_resources :sawmills
-      jsonapi_resources :score_operator_documents, only: [:index]
-      jsonapi_resources :severities, only: [:index, :show]
-      jsonapi_resources :species, only: [:index, :show]
-      jsonapi_resources :subcategories, only: [:index, :show]
-      jsonapi_resources :tools, only: [:index, :show]
-      jsonapi_resources :tutorials, only: [:index, :show]
-      jsonapi_resources :users
+      jsonapi_resources :observations do; end
+      jsonapi_resources :observation_documents do; end
+      jsonapi_resources :observation_reports do; end
+      jsonapi_resources :observers, only: [:index, :show] do; end
+      jsonapi_resources :operators do; end
+      jsonapi_resources :operator_documents, except: [:create, :update] do; end
+      jsonapi_resources :operator_document_annexes do; end
+      jsonapi_resources :operator_document_countries, except: [:create] do; end
+      jsonapi_resources :operator_document_fmus, except: [:create] do; end
+      jsonapi_resources :operator_document_histories do; end
+      jsonapi_resources :partners, only: [:index, :show] do; end
+      jsonapi_resources :required_gov_documents do; end
+      jsonapi_resources :required_operator_documents do; end
+      jsonapi_resources :required_operator_document_groups do; end
+      jsonapi_resources :sawmills do; end
+      jsonapi_resources :score_operator_documents, only: [:index] do; end
+      jsonapi_resources :severities, only: [:index, :show] do; end
+      jsonapi_resources :species, only: [:index, :show] do; end
+      jsonapi_resources :subcategories, only: [:index, :show] do; end
+      jsonapi_resources :tools, only: [:index, :show] do; end
+      jsonapi_resources :tutorials, only: [:index, :show] do; end
+      jsonapi_resources :users do; end
 
       resources :fmus, only: [:index, :update] do
         get 'tiles/:z/:x/:y', to: 'fmus#tiles', on: :collection
