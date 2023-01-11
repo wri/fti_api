c95db19b71ac933daf79a6eeb95eb612557fff8d
Tomasz Subik <tsubik@gmail.com>
2023-01-11 16:34:42 +0100

    fix wrong variable name in versionable

--- app/controllers/concerns/versionable.rb
+++ app/controllers/concerns/versionable.rb
@@ -7,15 +7,15 @@ module Versionable
 
       controller do
         def show
-          model = self.resource.class.base_class
-          var_name = self.resource.class.base_class.to_s.underscore
-
+          model = resource.class.base_class
           variable = model.includes(versions: :item).find(params[:id])
           @versions = variable.versions
-          if params[:version]
-            variable = variable.versions[params[:version].to_i].reify rescue variable
+          begin
+            variable = variable.versions[params[:version].to_i].reify if params[:version]
+          rescue StandardError => e
+            Sentry.capture_exception e
           end
-          instance_variable_set("@#{var_name}", variable)
+          instance_variable_set("@#{resource_instance_name}", variable)
           show! #it seems to need this
         end
       end
