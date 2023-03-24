b74ab647bcc3605e5e0d8801f48226adb47e09e3
Tomasz Subik <tsubik@gmail.com>
2023-03-24 11:44:22 +0100

    add system specs for dependent filters

--- lib/active_admin/dependent_filters_extension.rb
+++ lib/active_admin/dependent_filters_extension.rb
@@ -1,11 +1,11 @@
 module ActiveAdmin
   module DependentFiltersExtension
     def dependent_filters(&block)
-      filter_tree = block.call
-      filter_tree.each_value { |v| v.transform_values! { |collection| HashHelper.aggregate(collection) } }
-
       # sidebar won't be visible as css should hide it
       sidebar :dependent_filters, only: :index do
+        filter_tree = block.call
+        filter_tree.each_value { |v| v.transform_values! { |collection| HashHelper.aggregate(collection) } }
+
         render partial: 'dependent_filters', locals: { filter: filter_tree }
       end
     end
---
+++ spec/system/dependent_filters_spec.rb
@@ -0,0 +1,39 @@
+require 'system_helper'
+
+RSpec.describe 'Admin: Dependent Filters', type: :system do
+  let(:admin) { create(:admin) }
+
+  let!(:country1) { create(:country) }
+  let!(:country2) { create(:country) }
+  let!(:observer1) { create(:observer, countries: [country1]) }
+  let!(:observer2) { create(:observer, countries: [country1]) }
+  let!(:observer3) { create(:observer, countries: [country1], is_active: false) }
+  let!(:observer4) { create(:observer, countries: [country2]) }
+
+  before do
+    sign_in admin
+    visit '/admin/monitors'
+  end
+
+  it 'filters dependent select - test all features' do
+    # match all observers
+    expect(select2_options(from: 'Name')).to match_array([observer1.name, observer2.name, observer3.name, observer4.name])
+    # selecting country2 should match only observer 4
+    select2(country2.name, from: 'Country')
+    expect(select2_options(from: 'Name')).to match_array([observer4.name])
+    # select observer 4
+    select2(observer4.name, from: 'Name')
+    expect(select2_selected_options(from: 'Name')).to match_array([observer4.name])
+    # selecting country1 should clear observer(name) selection and match only observers 1, 2 and 3
+    select2(country1.name, from: 'Country')
+    expect(select2_selected_options(from: 'Name')).to match_array([''])
+    expect(select2_options(from: 'Name')).to match_array([observer1.name, observer2.name, observer3.name])
+    # select not active observer
+    select2(observer3.name, from: 'Name')
+    # change is active to false
+    select2('No', from: 'Is active')
+    # should keep the observer selected, but available options should be only observer 3
+    expect(select2_selected_options(from: 'Name')).to match_array([observer3.name])
+    expect(select2_options(from: 'Name')).to match_array([observer3.name])
+  end
+end
