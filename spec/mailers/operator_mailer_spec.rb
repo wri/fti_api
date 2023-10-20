3aad732025aa1871388481e069dfda927eb28ba8
Tomasz Subik <tsubik@gmail.com>
2023-10-20 17:05:30 +0200

    update operator mailers

--- spec/mailers/operator_mailer_spec.rb
+++ spec/mailers/operator_mailer_spec.rb
@@ -3,11 +3,12 @@ require "rails_helper"
 RSpec.describe OperatorMailer, type: :mailer do
   include ActionView::Helpers::UrlHelper
 
-  describe "expiring_documents_notifications" do
+  describe "expiring_documents" do
     let(:operator) { create(:operator) }
 
     let(:country) { create(:country) }
     let(:operator) { create(:operator, country_id: country.id, email: "test@mail.com") }
+    let(:user) { create(:operator_user, operator: operator) }
     let(:rod1) { create(:required_operator_document_country, country_id: country.id) }
     let(:rod2) { create(:required_operator_document_country, country_id: country.id) }
     let(:document1) {
@@ -17,11 +18,11 @@ RSpec.describe OperatorMailer, type: :mailer do
       create(:operator_document_country, required_operator_document_id: rod2.id, operator_id: operator, expire_date: Date.tomorrow)
     }
     let(:documents) { [document1, document2] }
-    let(:mail) { OperatorMailer.expiring_documents_notifications(operator, documents) }
+    let(:mail) { OperatorMailer.expiring_documents(operator, user, documents) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("You have 2 documents expiring in 1 day")
-      expect(mail.to).to eq([operator.email])
+      expect(mail.to).to eq([user.email])
     end
 
     it "renders the body" do
@@ -35,7 +36,7 @@ RSpec.describe OperatorMailer, type: :mailer do
     let(:mail) { OperatorMailer.quarterly_newsletter(operator, user) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Your quarterly OTP report / Votre rapport trimestriel sur l'OTP")
+      expect(mail.subject).to eq("Your quarterly OTP report")
       expect(mail.to).to eq([user.email])
     end
 
--- spec/mailers/previews/operator_mailer_preview.rb
+++ spec/mailers/previews/operator_mailer_preview.rb
@@ -1,13 +1,25 @@
 class OperatorMailerPreview < ActionMailer::Preview
-  def expiring_documents_notifications
-    OperatorMailer.expiring_documents_notifications operator, documents
-  end
-
   def quarterly_newsletter
     OperatorMailer.quarterly_newsletter operator, operator.users.filter_actives.first
   end
 
+  def expiring_documents
+    OperatorMailer.expiring_documents operator, operator.users.filter_actives.first, documents_expiring
+  end
+
+  def expired_documents
+    OperatorMailer.expired_documents operator, operator.users.filter_actives.first, documents_expired
+  end
+
   private
+
+  def documents_expired
+    OperatorDocument.where(operator_id: operator_id).doc_expired.last(3)
+  end
+
+  def documents_expiring
+    OperatorDocument.where(operator_id: operator_id).to_expire(1.month.from_now)
+  end
 
   def documents
     OperatorDocument.where(operator_id: operator_id).to_expire(1.month.from_now)
@@ -18,6 +30,9 @@ class OperatorMailerPreview < ActionMailer::Preview
   end
 
   def operator_id
-    OperatorDocument.to_expire(1.month.from_now).pluck(:operator_id).take(1).first
+    operators_with_expired = OperatorDocument.doc_expired.pluck(:operator_id).uniq
+    operators_with_expiring = OperatorDocument.to_expire(1.month.from_now).pluck(:operator_id).uniq
+
+    (operators_with_expired & operators_with_expiring).first
   end
 end
