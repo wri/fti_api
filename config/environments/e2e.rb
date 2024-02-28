b2cf62e04a5d240a18d3941aacc3170e7ec9fa48
Tomasz Subik <tsubik@gmail.com>
2024-02-28 11:37:50 +0100

    fix env configs

--- config/environments/e2e.rb
+++ config/environments/e2e.rb
@@ -6,13 +6,13 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
+  config.enable_reloading = false
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
-  config.enable_reloading = false
+  config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
