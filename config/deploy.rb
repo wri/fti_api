28935b22725e0b908f041a897fa352ce0fb09b64
Tomasz Subik <tsubik@gmail.com>
2024-07-16 16:18:41 +0200

    update config

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,29 +9,12 @@ set :repo_url, "git@github.com:wri/fti_api.git"
 ruby_version = File.read(".ruby-version").strip
 rvm_path = "/usr/share/rvm"
 
-# set :default_env, {
-#   "PATH" => "#{rvm_path}/gems/ruby-#{ruby_version}/bin:#{rvm_path}/bin:$PATH",
-#   "RUBY_VERSION" => "ruby-#{ruby_version}",
-#   "GEM_HOME" => "#{rvm_path}/gems/ruby-#{ruby_version}",
-#   "GEM_PATH" => "#{rvm_path}/gems/ruby-#{ruby_version}",
-#   "BUNDLE_PATH" => "#{rvm_path}/gems/ruby-#{ruby_version}"
-# }
-
-# set :passenger_restart_with_touch, true
-
 set :puma_threads, [4, 16]
 set :puma_workers, 0
-# set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-# set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-# set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-# set :puma_access_log, "#{release_path}/log/puma.access.log"
-# set :puma_error_log, "#{release_path}/log/puma.error.log"
 set :puma_service_unit_name, "puma"
-# set :puma_preload_app, true
-# set :puma_worker_timeout, nil
-# set :puma_init_active_record, true # Change to false when not using ActiveRecord
+set :puma_service_unit_env_vars, %w[RAILS_ENV=staging]
+
 set :systemctl_user, :system
-set :service_unit_env_vars, %w[RAILS_ENV=staging]
 
 set :rvm_type, :user
 set :rvm_ruby_version, ruby_version
@@ -117,10 +100,10 @@ end
 
 namespace :deploy do
   before :migrate, "deploy:db:load" if ENV["INITIAL"]
-  # after :starting, "sidekiq:quiet"
+  after :starting, "sidekiq:quiet"
   after :finishing, "deploy:cleanup"
-  # after :reverted, "sidekiq:restart"
-  # after :published, "sidekiq:restart"
+  after :reverted, "sidekiq:restart"
+  after :published, "sidekiq:restart"
 end
 
 
