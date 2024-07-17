f9dc316ea52b04b5059266bce1dddf4a5f6b54d6
Tomasz Subik <tsubik@gmail.com>
2024-07-17 16:42:08 +0200

    more updates

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -67,6 +67,5 @@ set :ssh_options, {
 }
 
 set :branch, ENV.fetch("BRANCH") { "staging" }
-# set :deploy_to, "/home/#{user}/fti-api"
-set :deploy_to , "/var/www/fti-api"
+set :deploy_to, "/var/www/otp-api"
 set :rails_env, "staging"
