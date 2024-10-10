7ad78a2068f1803574129e4837dbb3b4df8dc0de
Tomasz Subik <tsubik@gmail.com>
2024-10-10 11:28:40 +0200

    add non concession activity to observations

--- app/resources/v1/observation_resource.rb
+++ app/resources/v1/observation_resource.rb
@@ -11,7 +11,7 @@ module V1
     attributes :observation_type, :publication_date, :pv, :is_active,
       :details, :evidence_type, :evidence_on_report, :concern_opinion,
       :litigation_status, :location_accuracy, :lat, :lng, :country_id,
-      :fmu_id, :location_information, :subcategory_id, :severity_id,
+      :fmu_id, :non_concession_activity, :location_information, :subcategory_id, :severity_id,
       :created_at, :updated_at, :actions_taken, :validation_status, :validation_status_id,
       :is_physical_place, :hidden, :user_type, :monitor_comment, :locale
 
