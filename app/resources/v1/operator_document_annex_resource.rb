6d59e30a5dc38bb049dbf1dcea5bb319034b05b4
Tomasz Subik <tsubik@gmail.com>
2024-10-16 18:54:59 +0200

    use privateable

--- app/resources/v1/operator_document_annex_resource.rb
+++ app/resources/v1/operator_document_annex_resource.rb
@@ -4,10 +4,14 @@ module V1
   class OperatorDocumentAnnexResource < BaseResource
     include CacheableByLocale
     include CacheableByCurrentUser
+    include Privateable
+
     caching
     attributes :name,
       :start_date, :expire_date, :status, :attachment,
       :uploaded_by, :created_at, :updated_at
+
+    privateable :show_attributes?, [:name, :start_date, :expire_date, :status, :attachment, :uploaded_by, :created_at, :updated_at]
 
     has_one :operator_document, foreign_key_on: :related
 
@@ -25,38 +29,6 @@ module V1
       @model.annex_documents_history << adh
     end
 
-    def name
-      show_attribute("name")
-    end
-
-    def start_date
-      show_attribute("start_date")
-    end
-
-    def expire_date
-      show_attribute("expire_date")
-    end
-
-    def status
-      show_attribute("status")
-    end
-
-    def attachment
-      show_attribute("attachment")
-    end
-
-    def uploaded_by
-      show_attribute("uploaded_by")
-    end
-
-    def created_at
-      show_attribute("created_at")
-    end
-
-    def updated_at
-      show_attribute("updated_at")
-    end
-
     def set_user_id
       if context[:current_user].present?
         @model.user_id = context[:current_user].id
@@ -72,18 +44,14 @@ module V1
       @model.status = OperatorDocumentAnnex.statuses[:doc_pending]
     end
 
-    private
-
-    # TODO: This is a temporary solution until I don't find the problem
-    # with the caching of JsonApi Resources when some records are ignored
-    def show_attribute(attr)
-      if @model.status == "doc_valid" || belongs_to_user
-        @model.send(attr)
-      end
+    def show_attributes?
+      @model.status == "doc_valid" || belongs_to_user?
     end
 
+    private
+
     # TODO: Refactor this
-    def belongs_to_user
+    def belongs_to_user?
       user = context[:current_user]
       return true if user&.user_permission&.user_role == "admin"
 
