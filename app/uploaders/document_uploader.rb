846fa79246ceb7b34a1299139217e977a5c0cc73
Tomasz Subik <tsubik@gmail.com>
2025-11-06 09:49:29 +0100

    change uploaded documents store dir

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class DocumentUploader < ApplicationUploader
-  def store_dir
-    "uploads/documents/#{model.id}"
-  end
-
   def extension_allowlist
     %w[pdf doc docx txt csv xml jpg jpeg png exif tiff bmp]
   end
