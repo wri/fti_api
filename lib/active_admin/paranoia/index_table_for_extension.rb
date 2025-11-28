484ad23bdda5be7058049144e48eea3554bc4552
Tomasz Subik <tsubik@gmail.com>
2025-11-28 18:24:58 +0100

    integrate active admin paranoia extension into application

---
+++ lib/active_admin/paranoia/index_table_for_extension.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# base for this module taken from https://github.com/raihan2006i/active_admin_paranoia
+
+module ActiveAdmin
+  module Paranoia
+    module IndexTableForExtension
+      extend ActiveSupport::Concern
+
+      included do
+        alias_method :orig_defaults, :defaults
+
+        def defaults(resource, options = {})
+          if resource.respond_to?(:deleted?) && resource.deleted?
+            if controller.action_methods.include?("restore") && authorized?(:restore, resource)
+              # TODO: find a way to use the correct path helper
+              item I18n.t("active_admin_paranoia.restore"), "#{resource_path(resource)}/restore", method: :put, class: "restore_link #{options[:css_class]}",
+                data: {confirm: I18n.t("active_admin_paranoia.restore_confirmation")}
+            end
+          else
+            orig_defaults(resource, options)
+          end
+        end
+      end
+    end
+  end
+end
