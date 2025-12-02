b1dc419ce08e0cbdfa2f403cb28995cf4839d323
Tomasz Subik <tsubik@gmail.com>
2025-12-02 16:26:47 +0100

    update cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins "*"
-    resource "*", headers: :any, methods: %i[get post put patch delete options head]
+if Rails.env.development? || Rails.env.e2e?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins(/^http:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/)
+      resource "*", headers: :any, methods: %i[get post put patch delete options head]
+    end
   end
 end
