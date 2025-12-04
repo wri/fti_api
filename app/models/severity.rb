b8620b2b5247844515857c2fb610f29d641c0b0e
Tomasz Subik <tsubik@gmail.com>
2025-12-04 10:02:31 +0100

    remove unused method

--- app/models/severity.rb
+++ app/models/severity.rb
@@ -23,10 +23,6 @@ class Severity < ApplicationRecord
   belongs_to :subcategory, inverse_of: :severities
   has_many :observations, inverse_of: :severity
 
-  def level_details
-    "#{level} - #{details}"
-  end
-
   validates :level, presence: true
   validates :level, uniqueness: {scope: :subcategory_id}
   validates :level, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 3}
