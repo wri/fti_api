ad650051f9719a3fb45ce65667a705c94b3fc672
Tiago Santos <santos.tiago@gmail.com>
2026-04-30 17:36:21 +0200

    Notify users that they haven't login in a while and deactivate accounts
    # Conflicts:
    #	config/schedule.rb

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -30,4 +30,31 @@ RSpec.describe UserMailer, type: :mailer do
       expect(mail.body.encoded).to match(I18n.t("user_mailer.forgotten_password.message"))
     end
   end
+
+  describe "inactive_account_warning" do
+    let(:disable_date) { Date.new(2028, 1, 1) }
+    let(:mail) { UserMailer.inactive_account_warning(user, disable_date) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq(I18n.t("user_mailer.inactive_account_warning.subject", disable_date: I18n.l(disable_date)))
+      expect(mail.to).to eq([user.email])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match(I18n.t("user_mailer.inactive_account_warning.message", disable_date: I18n.l(disable_date)))
+    end
+  end
+
+  describe "account_deactivated_for_inactivity" do
+    let(:mail) { UserMailer.account_deactivated_for_inactivity(user) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq(I18n.t("user_mailer.account_deactivated_for_inactivity.subject"))
+      expect(mail.to).to eq([user.email])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match(I18n.t("user_mailer.account_deactivated_for_inactivity.message"))
+    end
+  end
 end
